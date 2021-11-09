0617080b64f1525a306a521d278a71fb1470fe1f
jobAbraham1 <jobabraham85@gmail.com>
2021-11-08 20:55:59 -0500

    added collapsible

--- Java Script/index.js
+++ Java Script/index.js
@@ -1,1 +1,6 @@
-javascript
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
